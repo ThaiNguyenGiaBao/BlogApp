404b3a81511d6c1fa9b965f4b26ca9f9ba47b69c
Gia Bao <thainguyengiabao27092004@gmail.com>
2024-07-22 23:56:08 +0700

    add persist

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
 import Theme from "./components/ThemeProvider";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <Theme>
-      <App />
-    </Theme>
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <Theme>
+        <App />
+      </Theme>
+    </Provider>
+  </PersistGate>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,27 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
 import themeReducer from './theme/themeSlice';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    theme: themeReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        theme: themeReducer
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
 })
+
+export const persistor = persistStore(store);
